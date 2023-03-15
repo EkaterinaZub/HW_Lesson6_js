df74d9e2cfba62284c175c4a98bfb42b716698ed
EkaterinaZub <katenka.zu@gmail.com>
2023-03-15 11:58:01 +0300

    add: tasks added 1,2

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 "Use strict"
+/  Task 1
+// Дана строка 'aaa@bbb@ccc'. Замените все @ на ! с помощью глобального
+// поиска и замены.
+{
+    let str = `aaa@bbb@ccc`;
+    console.log(str.replace(/@/gi, `!`));
+}
+
+// Task 2
+// В переменной date лежит дата в формате 2025-12-31. Преобразуйте эту
+// дату в формат 31/12/2025.
+{
+    let time = `2025-12-31`;
+     // console.log(time.replace(/-/gi, `/`));
+    console.log(time.replace(/(2025)(-)(12)(-)(31)/, `$5/$3/$1`));
+   
+}
+// Task 3
+// Дана строка «Я учу javascript!». Вырежете из нее слово «учу» и слово
+// «javascript» тремя разными способами (через substr, substring, slice).
